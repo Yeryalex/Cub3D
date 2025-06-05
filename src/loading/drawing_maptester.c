1a1a7e2df6c65048b77cd3e35e31ec1991c9ad82
WeBeRATAS <rbuitrag@WEBER-laptpop.localdomain>
2025-06-05 19:56:24 +0200

    Create draw_maptester for maths integrates textures

--- src/loading/drawing_maptester.c
+++ src/loading/drawing_maptester.c
@@ -6,7 +6,7 @@
 /*   By: rbuitrag <rbuitrag@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 10:28:15 by rbuitrag          #+#    #+#             */
-/*   Updated: 2025/06/05 19:53:12 by rbuitrag         ###   ########.fr       */
+/*   Updated: 2025/06/05 19:38:14 by rbuitrag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,33 +100,110 @@ void	clear_image(t_mlx_vars *vars)
 }
 
 
+int	get_texture_color(t_texture *tex, int x, int y)
+{
+    int	offset;
 
+	if (!tex || !tex->addr)
+		return (0x000000); 
+    if (x < 0)
+        x = 0;
+    if (y < 0)
+        y = 0;
+    if (x >= tex->width)
+        x = tex->width - 1;
+    if (y >= tex->height)
+        y = tex->height - 1;
+    offset = y * tex->line_length + x * (tex->bits_per_pixel / 8);
+    return (*(int *)(tex->addr + offset));
+}
+
+t_texture	*select_wall_texture(t_mlx_vars *vars, int side, double ray_dir_x, double ray_dir_y)
+{
+    if (side == 0)
+    {
+        if (ray_dir_x > 0)
+            return (&vars->config.east_tex);
+        else
+            return (&vars->config.west_tex);
+    }
+    else
+    {
+        if (ray_dir_y > 0)
+            return (&vars->config.south_tex);
+        else
+            return (&vars->config.north_tex);
+    }
+}
+
+void	draw_wall_texture(t_mlx_vars *vars, int x, double start_y, double height, t_texture *tex, double wall_x)
+{
+    int		y;
+    int		tex_x;
+    int		tex_y;
+    double	step;
+    double	tex_pos;
+
+	if (!tex || !tex->addr)
+        return; 
+    tex_x = (int)(wall_x * (double)tex->width);
+    step = 1.0 * tex->height / height;
+    tex_pos = 0.0;
+    y = (int)start_y;
+    while (y < start_y + height && y < RES_WINHEIGHT)
+    {
+        tex_y = (int)tex_pos;
+        put_pixel(x, y, get_texture_color(tex, tex_x, tex_y), vars);
+        tex_pos += step;
+        y++;
+    }
+}
+
+// Sustituye tu draw_line por esta versión:
 void	draw_line(t_mlx_vars *vars, double start_x, int i)
 {
-	double	ray_x = vars->config.player.pos_x;
-	double	ray_y = vars->config.player.pos_y;
-	double	cos_angle = cos(start_x);
-	double  sin_angle = sin(start_x);
+    double	ray_x = vars->config.player.pos_x;
+    double	ray_y = vars->config.player.pos_y;
+    double	cos_angle = cos(start_x);
+    double  sin_angle = sin(start_x);
+    int side = 0;
+    double	step = 1.0;
+    double	wall_x;
+    double	dist_x, dist_y;
 
-	while (!ft_make_contact(ray_x, ray_y, vars))
-	{
-		if (PLANES)
-			put_pixel(ray_x, ray_y, vars->config.floor_color.combined, vars);
-		ray_x += cos_angle;
-		ray_y += sin_angle;
-	}
-	if (!PLANES)
-	{
-		double dist = fixed_dist(vars->config.player.pos_x, vars->config.player.pos_y, ray_x, ray_y, vars);
-		double height = (64 / dist) * (RES_WINWIDHT/ 2);
-		double start_y = (RES_WINHEIGHT - height) / 2;
-		int end = start_y + height;
-		while (start_y < end)
-		{
-			put_pixel(i, start_y, 0x59614A, vars);
-			start_y++;
-		}
-	}
+    // DDA simple para encontrar la pared y el lado de impacto
+    while (!ft_make_contact(ray_x, ray_y, vars))
+    {
+        dist_x = fabs((floor(ray_x / 64 + 1) * 64 - ray_x) / cos_angle);
+        dist_y = fabs((floor(ray_y / 64 + 1) * 64 - ray_y) / sin_angle);
+        if (dist_x < dist_y)
+        {
+            ray_x += cos_angle * step;
+            side = 0;
+        }
+        else
+        {
+            ray_y += sin_angle * step;
+            side = 1;
+        }
+        ray_x += cos_angle * step;
+        ray_y += sin_angle * step;
+    }
+    if (!PLANES)
+    {
+        double dist = fixed_dist(vars->config.player.pos_x, vars->config.player.pos_y, ray_x, ray_y, vars);
+        double height = (64 / dist) * (RES_WINWIDHT / 2);
+        double start_y = (RES_WINHEIGHT - height) / 2;
+
+        // Calcular wall_x (pos de impacto en la pared)
+        if (side == 0)
+            wall_x = fmod(ray_y / 64.0, 1.0);
+        else
+            wall_x = fmod(ray_x / 64.0, 1.0);
+
+        t_texture *tex = select_wall_texture(vars, side, cos_angle, sin_angle);
+        draw_wall_texture(vars, i, start_y, height, tex, wall_x);
+    }
 }
 
 
@@ -139,7 +216,6 @@ int	drawing_loop(t_mlx_vars *vars)
 	clear_image(vars);
 	if (PLANES)
 	{
-		//draw_square(vars->config.player.pos_x, vars->config.player.pos_y, 10, 0x00FF00, vars->config.floor_color.combined, vars);
 		draw_square(vars->config.player.pos_x, vars->config.player.pos_y, 10, vars->config.floor_color.combined, vars);
 		draw_map(vars);
 	}
